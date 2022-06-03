8a7dcd97282d2642cd2921592ebfbb72a45de537
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-06-04 02:53:39 +0300

    fixed import error

--- IG/forms.py
+++ IG/forms.py
@@ -3,7 +3,7 @@ from django.db import models
 import datetime as dt
 from django.contrib.auth.models import User
 from django.forms import ModelForm, widgets
-from django.dispatch import receiver
+from django import forms
 from django.db.models.signals import post_save
 from .models import Image, Profile, Comments
 
--- IG/models.py
+++ IG/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django import forms
 import datetime as dt
 from django.contrib.auth.models import User
-from django.forms import ModelForm, widgets
+from django.forms import widgets
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 # from cloudinary.models import CloudinaryField
